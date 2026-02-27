00120f5183821097b39e5dfd2190a17568e8629b
kacem chaima <chaima1306kacem@gmail.com>
2026-02-27 14:47:21 +0100

    testing files

---
+++ test/BigOSymb.asm
@@ -0,0 +1,21 @@
+// For Language theory
+// 2025-2026
+// 2 LMD
+// BigOSymb.asm
+
+@0
+D=M
+@1
+D=D-M
+@10
+D;JGT
+@1
+D=M
+@12
+0;JMP
+@0
+D=M
+@2
+M=D
+@14
+0;JMP
---
+++ test/plus.asm
@@ -0,0 +1,13 @@
+// For Language theory
+// 2025-2026
+// 2 LMD
+// Plus.asm
+
+// Computes R0 = 2 + 3  (R0 refers to RAM[0])
+
+@2
+D=A
+@3
+D=D+A
+@0
+M=D
